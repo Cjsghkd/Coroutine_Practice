641d93cd81154942b66bda2a37f955dc470ceefb
Cjsghkd <pjhomg5530@naver.com>
2023-10-04 17:10:58 +0900

    :sparkles: :: 예제 추가

---
+++ app/src/main/java/com/example/coroutine_practice/ch_1_1/example_3.kt
@@ -0,0 +1,12 @@
+package com.example.coroutine_practice.ch_1_1
+
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+//    println(coroutineContext)
+    println(this.coroutineContext)
+    println(Thread.currentThread().name)
+    println("Hello")
+}
+
+// 코루틴 스코프는 코루틴을 제대로 처리하기 위한 정보, 코루틴 컨텍스트(CoroutineContext)를 가지고 있습니다.
